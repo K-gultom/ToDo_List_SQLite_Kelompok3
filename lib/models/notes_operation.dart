65bb9999443949c82ad52bc433cac01d41796484
RenSaputri <yusefi.berlian@gmail.com>
2023-06-07 16:46:21 +0700

    note operation

--- lib/models/notes_operation.dart
+++ lib/models/notes_operation.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/foundation.dart';
-import 'package:todo_list/models/note.dart';
+import 'package:todo_list/Models/note.dart';
+import 'package:todo_list/Helpers/DatabaseHelper.dart';
 
 class NotesOperation with ChangeNotifier {
   List<Note> _notes = [];
   List<Note> get getNotes => _notes;
 
-  void addNewNote(String title, String description) {
-    Note note = Note(title, description);
+  Future<void> addNewNote(String title, String description) async {
+    Note note = Note(
+      title: title,
+      description: description,
+    );
+    int id = await DatabaseHelper.instance.insert(note);
+    note.id = id;
     _notes.add(note);
-    notifyListeners(); //untuk memberitahu listener yang akan digunakan
+    notifyListeners();
+  }
+
+  Future<void> getNotesFromDatabase() async {
+    List<Note> notes = await DatabaseHelper.instance.getNotes();
+    _notes = notes;
+    notifyListeners();
+  }
+
+  Future<void> updateNote(Note note) async {
+    await DatabaseHelper.instance.update(note);
+    int index = _notes.indexWhere((n) => n.id == note.id);
+    if (index != -1) {
+      _notes[index] = note;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteNote(Note note) async {
+    await DatabaseHelper.instance.delete(note);
+    _notes.removeWhere((n) => n.id == note.id);
+    notifyListeners();
   }
 }
